2154ec8455102e1b7850d3207313bc4fb35f74a2
James Hilts <jameshilts@Jamess-MacBook-Pro.local>
2024-03-05 16:13:42 -0800

    Added icon attribution.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,6 +3,11 @@ export default function Footer() {
 		<>
 		<section id="footer">
 		<p>&copy; 2024 Created by James Hilts</p>
+		<p>Atrribution</p>
+		<p className="attribution">LinkIn Icon by
+		<a className="link_pro" href="https://freeicons.io/essential-web-1/linkedin-social-media-logo-website-icon-40217">BECRIS on</a>
+		<a className="attribution" href="https://freeicons.io">freeicons.io</a>
+		</p>
 		</section>
 		</>
 		);
