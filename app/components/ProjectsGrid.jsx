7c9cdc3fa039afc400d5afe33b5fd620feb525b8
James Hilts <jameshilts@Jamess-MacBook-Pro.local>
2024-03-07 16:52:35 -0800

    Added link to technical document project.

--- app/components/ProjectsGrid.jsx
+++ app/components/ProjectsGrid.jsx
@@ -4,24 +4,24 @@ export default function ProjectsGrid() {
 	return(
 		<div className="flex-container">
 		<h1>Here are some of my projects</h1>
-		<Link to="https://seahorse-app-a6kdz.ondigitalocean.app/">
+		<Link href="https://seahorse-app-a6kdz.ondigitalocean.app/">
 		<div className="card">
 		<img  id="project-img" src="images/penguin.png" alt="Penguin Waving Scene"/>
 		<p id="project-desciption">Pengiun Waving</p>
 		</div>
 		</Link>
-		<a href="">
+		<Link href="">
 		<div className="card">
 		<img id="project-img" src="images/jhilts-landing-page.jpg" alt="Landing Page For J Hilts"/>
 		<p id="project-desciption">Landing Page</p>
 		</div>
-		</a>
-		<a href="">
+		</Link>
+		<Link href="https://oyster-app-ctd42.ondigitalocean.app/">
 		<div className="card">
 		<img id="project-img" src="images/technical-document.jpg" alt="Technical Document"/>
 		<p id="project-desciption">Technical Document</p>
 		</div>
-		</a>
+		</Link>
 		</div>
 		);
 }
